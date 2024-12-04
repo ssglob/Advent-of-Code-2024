c89d4a1f2d03e4f88dc3b14e8585333ba38a087e
ssglob <70921214+ssglob@users.noreply.github.com>
2024-12-03 23:50:19 -0800

    Finished Day 3

--- Day 3/Day 3.py
+++ Day 3/Day 3.py
@@ -1,17 +1,21 @@
+# Part 1
 f = open('Advent of Code 2024\\Day 3\\Day 3 input.txt','r')
 result = 0
 for line in f:
     for c,char in enumerate(line):
         if char != 'm':
             continue
-        if c > len(line)-9:
+        if c > len(line)-8:
             break
         if line[c:c+4] != 'mul(':
             continue
         count = c+4
         nums = []
         cur_string = ''
-        while count<=len(line)-1 and line[count] in '1234567890,)' and len(cur_string)<=3:
+        while count<=len(line)-1 and len(cur_string)<=3:
+            if not line[count] in '1234567890,)':
+                nums.clear()
+                break
             if line[count] == ',':
                 nums.append(int(cur_string))
                 cur_string = ''
@@ -23,5 +27,44 @@ for line in f:
             count += 1
         if len(nums) == 2:
             result += nums[0] * nums[1]
+print(result)
+f.close()
+
+#Part 2
+f = open('Advent of Code 2024\\Day 3\\Day 3 input.txt','r')
+result = 0
+do = True
+for line in f:
+    for c,char in enumerate(line):
+        if char == 'd':
+            if c < len(line)-5 and line[c:c+4] == 'do()':
+                do = True
+            if c < len(line)-8 and line[c:c+7] == 'don\'t()':
+                do = False
+        if do:
+            if char != 'm':
+                continue
+            if c > len(line)-8:
+                break
+            if line[c:c+4] != 'mul(':
+                continue
+            count = c+4
+            nums = []
+            cur_string = ''
+            while count<=len(line)-1 and len(cur_string)<=3:
+                if not line[count] in '1234567890,)':
+                    nums.clear()
+                    break
+                if line[count] == ',':
+                    nums.append(int(cur_string))
+                    cur_string = ''
+                elif line[count] == ')':
+                    nums.append(int(cur_string))
+                    break
+                else:
+                    cur_string += line[count]
+                count += 1
+            if len(nums) == 2:
+                result += nums[0] * nums[1]
 print(result)
 f.close()
